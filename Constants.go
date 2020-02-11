113b15a961eef2e0c02887cdd0be7c90d7999a99
jojiiOfficial <jojii@jojii.de>
2020-02-11 22:11:08 +0100

    add endpoints

--- Constants.go
+++ Constants.go
@@ -7,6 +7,24 @@ const (
 	DataDir           = ".whsub"
 )
 
+//Endpoints
+const (
+	//Subscriptions
+	EPSubscription         = "/sub"
+	EPSubscriptionAdd      = EPSubscription + "/add"
+	EPSubscriptionActivate = EPSubscription + "/activate"
+	EPSubscriptionRemove   = EPSubscription + "/remove"
+
+	//User
+	EPUser       = "/user"
+	EPUserCreate = EPUser + "/create"
+
+	//Source
+	EPSource       = "/source"
+	EPSourceCreate = EPSource + "/create"
+	EPSourceDelete = EPSource + "/delete"
+)
+
 var (
 	//DefaultDatabaseFile database file
 	DefaultDatabaseFile = "data.db"
